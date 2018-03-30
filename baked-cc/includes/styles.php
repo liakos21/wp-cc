e1ecd3910fe9ef066cc48b71853f3a689d741ebe
liakos21 <iloulakakis@gmail.com>
2018-03-30 16:34:12 +0300

    Fresh Commit
    This reverts commit 98469b28e4d1fd11925b4e938886c46dea347bd7.

--- baked-cc/includes/styles.php
+++
@@ -1,9 +0,0 @@
-<?php
-add_action( 'wp_enqueue_scripts', 'baked_cc_assets' );
-
-function baked_cc_assets() {
-	if (get_option('baked_cc_switcher')) {
-		wp_enqueue_style( 'baked-cc-css', plugin_dir_url( __FILE__ ) . '../css/cookieconsent.min.css', array() );
-		wp_enqueue_script( 'baked-cc-js', plugin_dir_url( __FILE__ ) . '../js/cookieconsent.min.js', array(), false );
-	}
-}
--- baked-cc/includes/switchers.php
+++
@@ -1,6 +0,0 @@
-<?php
-function display_baked_cc_switcher() {
-	?>
-    	<input type="checkbox" name="baked_cc_switcher" id="baked_cc_switcher" value="1" <?php checked(1, get_option('baked_cc_switcher'), true); ?> />
-    <?php
-}
